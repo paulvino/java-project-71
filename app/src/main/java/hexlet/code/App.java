70700fe3399db30e0542ef79f511809bab89de3b
Pavel Vinogradov <paul.vin.95@gmail.com>
2023-07-25 22:36:16 +0300

    finished step 4, upd App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -14,6 +14,16 @@ import java.util.concurrent.Callable;
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
         description = "Compares two configuration files and shows a difference.")
 class App implements Callable<Integer> {
+    private static final int SUCCESS_EXIT_CODE = 0;
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filePath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to the second file")
+    private String filePath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish", description = "output format [default: stylish]")
+    private String format;
 
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     boolean usageHelpRequested;
@@ -23,16 +33,11 @@ class App implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        return 0;
+        return SUCCESS_EXIT_CODE;
     }
 
     public static void main(String... args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
-
-//public class App {
-//    public static void main(String[] args) {
-//        System.out.println("Hello, World!");
-//    }
 }
